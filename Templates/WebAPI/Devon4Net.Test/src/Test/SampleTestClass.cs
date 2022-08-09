5a66a9a7a52389d1694d6c21abd1a100bb0019ce
Moukhli <othman.moukhli@capgemini.com>
2022-08-09 20:55:34 +0200

    Used devon test template to generate test project (source: https://github.com/devonfw/devon4net/blob/39b60e3d04b7dfcfe0bda3f4380d75648fc631ac/documentation/packages.asciidoc#devon4netinfrastructuretest) changed TargetFramework to net6.0, and corrected versions of package references in Devon4Net.Test.csproj

---
+++ Templates/WebAPI/Devon4Net.Test/src/Test/SampleTestClass.cs
@@ -0,0 +1,42 @@
+﻿using Devon4Net.Test.xUnit.Test.Integration;
+using Devon4Net.Application.WebAPI.Implementation.Domain.Entities;
+
+using Xunit;
+
+namespace Devon4Net.Test.xUnit
+{
+    public class SampleTestClass: IntegrationTest
+    {
+        [Fact]
+        public void PassingTest()
+        {   
+            var user = new User();
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
+    }
+}
