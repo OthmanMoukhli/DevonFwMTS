4c3f4a9f11edc82ff2d09b91c86862ef1536d4b9
Moukhli <othman.moukhli@capgemini.com>
2022-08-17 16:02:44 +0200

    DishControllerTest 2nd Branch when input is null

--- Templates/WebAPI/Devon4Net.Test/src/Test/UnitTest/Business/DishManagement/Controllers/DishControllerTest.cs
+++ Templates/WebAPI/Devon4Net.Test/src/Test/UnitTest/Business/DishManagement/Controllers/DishControllerTest.cs
@@ -18,7 +18,7 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
     public class DishControllerTest: UnitTest
     {
         [Fact]
-        public async void DishSearchTest()
+        public async void DishSearch_Returns_Result()
         {   
             //Arrange
             List<Dish> dishList = new List<Dish>();
@@ -71,6 +71,43 @@ namespace Devon4Net.Test.xUnit.Test.UnitTest.Business
             //Act
             var result =(ObjectResult) await dishController.DishSearch(input);
             
+            //Assert
+            dishServiceInterfaceMock.Verify(s => s.GetDishesMatchingCriterias(
+                    It.IsAny<decimal>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string>(),
+                    It.IsAny<IList<long>>()), Times.Once());
+            
+            Assert.Equal(expectedResult,result.Value);
+        }
+
+        [Fact]
+        public async void DishSearch_Null_Returns_Default()
+        {   
+            //Arrange
+            Dish dish1 = new Dish();
+            dish1.Name = "burger";
+            dish1.Price = 6;
+
+            List<Dish> dishList = new List<Dish>();
+            dishList.Add(dish1);
+
+            var dishServiceInterfaceMock = new Mock<IDishService>();
+            dishServiceInterfaceMock.Setup(
+                s => s.GetDishesMatchingCriterias(
+                    It.IsAny<decimal>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string>(),
+                    It.IsAny<IList<long>>())
+                ).Returns(
+                    Task.FromResult(dishList)
+            );
+            DishController dishController = new DishController(dishServiceInterfaceMock.Object);
+                
+            var expectedResult = @"{""pagination"":{""size"":0,""page"":0,""total"":1},""content"":[{""dish"":{""id"":0,""modificationCounter"":0,""revision"":null,""name"":""burger"",""description"":null,""price"":6.0,""imageId"":null},""image"":{""id"":0,""modificationCounter"":null,""revision"":null,""name"":null,""content"":null,""contentType"":null,""mimeType"":null},""test"":null,""extras"":[],""categories"":[]}]}";
+            
+            //Act
+            var result =(ObjectResult) await dishController.DishSearch(null);
             //Assert
             dishServiceInterfaceMock.Verify(s => s.GetDishesMatchingCriterias(
                     It.IsAny<decimal>(),
