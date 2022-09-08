140ec4e9aaa2ef958202f80c4007ab9505b2eb09
BenjaminE <benjamin.ehnes@fh-erfurt.de>
2022-09-08 10:30:14 +0200

    cleanup of deps, got rid of management-tests, added comment for global tool

--- CodeCoverage/create_report_script.cmd
+++ CodeCoverage/create_report_script.cmd
@@ -1,6 +1,8 @@
 :: runs commands to test and generate code coverage report 
 :: place in /<RootProjectDirectory>/CodeCoverage
 :: use terminal to run script
+:: this script uses the tool dotnet-reportgenerator-globaltool
+:: please install it using dotnet tool install -g dotnet-reportgenerator-globaltool
 cd %~dp0 
 cd..
 set "projectPath=%CD%"
@@ -13,11 +15,9 @@ set excludeClasses=-Program;^
 -Devon4Net.Application.WebAPI.Implementation.Business.AntiForgeryTokenManagement.*;^
 -Devon4Net.Application.WebAPI.Implementation.Business.AuthManagement.*;^
 -Devon4Net.Application.WebAPI.Implementation.Business.CyberArkManagement.*;^
--Devon4Net.Application.WebAPI.Implementation.Business.EmployeeManagement.*;^
 -Devon4Net.Application.WebAPI.Implementation.Business.MediatRManagement.*;^
 -Devon4Net.Application.WebAPI.Implementation.Business.RabbitMqManagement.*;^
 -Devon4Net.Application.WebAPI.Implementation.Business.SmaxHcmrManagement.*;^
--Devon4Net.Application.WebAPI.Implementation.Business.TodoManagement.*;^
 -Devon4Net.Application.WebAPI.Implementation.Business.DishManagement.Dto.ExtraDto;^
 -Devon4Net.Application.WebAPI.Implementation.Business.DishManagement.Dto.CategoryDto;^
 -Devon4Net.Application.WebAPI.Implementation.Business.CategoryManagement.Dto.CategoryDto;^
@@ -26,5 +26,5 @@ set excludeClasses=-Program;^
 cd %projectPath%\Templates\WebAPI\Devon4Net.Test 
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]*" /p:CoverletOutput="%projectPath%\CodeCoverage\test_result\coverage.cobertura.xml" 
 reportgenerator "-reports:%projectPath%\CodeCoverage\test_result\coverage.cobertura.xml" "-targetdir:%projectPath%\CodeCoverage\report" "-classfilters:%excludeClasses%" 
-start %projectPath%\CodeCoverage\report\index.html
-pause
+:: start %projectPath%\CodeCoverage\report\index.html
+:: pause
--- Templates/WebAPI/Devon4Net.Test/src/Database/ModelContext.cs
+++ Templates/WebAPI/Devon4Net.Test/src/Database/ModelContext.cs
@@ -6,6 +6,5 @@ namespace Devon4Net.Domain.Entities
     public class ModelContext : Devon4NetBaseContext
     {
         public ModelContext(DbContextOptions<ModelContext> options) : base(options) { }
-
     }
 }
